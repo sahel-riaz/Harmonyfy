8ad88aacb401d998d87dfbe125133eb2fc885a2a
Iroh02 <nanditam1010@gmail.com>
2023-11-07 00:00:12 +0530

    Update: Fixerd load_data.py

--- load_data.py
+++ load_data.py
@@ -3,23 +3,28 @@ import pretty_midi
 import csv
 import numpy as np
 import json
+import math
 
 # Import the functions and variables from your original script
 from encoding import encode, save_csv
 
 def main():
-    maestro_dir = "maestro-v1.0.0"
+    maestro_dir = "maestro-v2.0.0"
     csv_tuple_dir = "csv_tuple"
+    target_file = '35393.mid'
     
     if not os.path.exists(csv_tuple_dir):
         os.makedirs(csv_tuple_dir)
 
     for root, _, files in os.walk(maestro_dir):
         for file in files:
-            if file.endswith(".midi"):
+            if file == target_file:
                 midi_file = os.path.join(root, file)
                 music = pretty_midi.PrettyMIDI(midi_file)
-                encoded = encode(music)
+                tempo_changes = music.get_tempo_changes()
+                tempo = math.floor(tempo_changes[1][0])
+                
+                encoded = encode(music, tempo)
                 encoded_data = encoded.tolist()
                 save_csv(os.path.join(csv_tuple_dir, f"{os.path.splitext(file)[0]}.csv"), encoded_data)
 
