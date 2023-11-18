d4ded74cf5dac655e45280c9a6640bb10521926a
Anagh Deepak <anaghdeepak@gmail.com>
2023-11-18 16:56:13 +0530

    fully done encoding and load data

--- load_data.py
+++ load_data.py
@@ -1,24 +1,37 @@
 import os
-import pretty_midi
-import csv
+import muspy
 import numpy as np
-import json
 
-from encoding import encode, save_npz
+import muspy_encoding_data as encoding
+
+def write_numpy_to_txt(numpy_dir, output_txt_dir):
+    for root, _, files in os.walk(numpy_dir):
+        for file in files:
+            if file.endswith(".npy"):
+                npy_file = os.path.join(root, file)
+                data = np.load(npy_file)
+                output_txt = os.path.join(output_txt_dir, f"{file}.txt")
+                np.savetxt(output_txt, data, fmt='%d', delimiter='\t')
 
 def main():
-    maestro_dir = "maestro-v2.0.0"
-    npy_dir = "numpy_representation"  
-    
-    if not os.path.exists(npy_dir):
-        os.makedirs(npy_dir)
+    maestro_dir = "maestro-v3.0.0"
+    music_numpy_dir = "muspy_numpy_dir"
+    music_numpy_txt = 'muspy_numpy_txt'
+
+    if not os.path.exists(music_numpy_dir):
+        os.makedirs(music_numpy_dir)
+
+    if not os.path.exists(music_numpy_txt):
+        os.makedirs(music_numpy_txt)
+
     for root, _, files in os.walk(maestro_dir):
         for file in files:
             if file.endswith(".midi"):
                 midi_file = os.path.join(root, file)
-                music = pretty_midi.PrettyMIDI(midi_file)
-                encoded = encode(music)
-                npy_file = os.path.join(npy_dir, f"{os.path.splitext(file)[0]}.npy")
-                np.save(npy_file, encoded)
+                music = muspy.read(midi_file)
+                numpy_data = encoding.convert_to_numpy(music)
+                np.save(os.path.join(music_numpy_dir, f"{file}.npy"), numpy_data)
+                print('---------------------- NumPy DONE ---------------------')
+
 if __name__ == "__main__":
     main()
