5d73c5de35f5eaee7f288d689479bbfa237f6560
Anagh Deepak <anaghdeepak@gmail.com>
2023-12-02 19:40:06 +0530

    load data with train test split

--- load_data.py
+++ load_data.py
@@ -1,37 +1,38 @@
 import os
 import muspy
 import numpy as np
-import pandas as pd
-import encoding as encoding
+import shutil
+from sklearn.model_selection import train_test_split
+import encoding1 as encoding
 
 def main():
+
     maestro_dir = "maestro-v2.0.0"
-    music_numpy_dir = "muspy_numpy_dir"
+    train_path = "train_dataset"
+    validation_path = "validation"
 
-    if not os.path.exists(music_numpy_dir):
-        os.makedirs(music_numpy_dir)
-    res = []
+    if not os.path.exists(train_path):
+        os.makedirs(train_path)
+
     for root, _, files in os.walk(maestro_dir):
-        i = 0
-        for file in files:
+        for i, file in enumerate(files):
             if file.endswith(".midi"):
-                i += 1
-                if os.path.exists(os.path.join(music_numpy_dir, f"{file}.npy")):
-                    continue
-
                 midi_file = os.path.join(root, file)
                 music = muspy.read(midi_file)
-                flag = 0
-                flag, max_beat, max_dur, total = encoding.constraint_checker(music)
-                print(max_beat, max_dur, total)
-                # res.append([max_beat, max_dur, total])
-                if flag: continue
-
+                
                 numpy_data = encoding.convert_to_numpy(music)
-                np.save(os.path.join(music_numpy_dir, f"{file}.npy"), numpy_data)
-                print('{}---------------------- NumPy DONE ---------------------'.format(i))
-    # df = pd.DataFrame(res, columns=['max_beat', 'max_dur', 'total'])
-    # df.to_csv('statistics.csv', index=False)
+                if numpy_data is None:
+                    continue
+                else:
+                    np.save(os.path.join(train_path, f"{file}.npy"), numpy_data)
+                    print('----------------------', i, 'NumPy DONE ---------------------')
+    
+    # split into train and validation
+    file_paths = [file for file in os.listdir(train_path) if file.endswith(".npy")]
+
+    _, val_files = train_test_split(file_paths, test_size=0.1, random_state=42)
+
+    move_files(val_files, train_path, validation_path)
 
 if __name__ == "__main__":
     main()
